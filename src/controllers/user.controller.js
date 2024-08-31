084a11a06a8b2c352954caddec6a9817299006b8
Vedant Warhekar <vwarhekar444@gmail.com>
2024-08-31 17:45:06 +1200

    added functionality of user Watch History

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -408,6 +408,60 @@ const getUserChannelProfile = asynchandler(async () => {
     .json(new ApiResponse(200, channel[0], "User channel subscryebd"));
 });
 
+const getWatchHistory = asynchandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -419,4 +473,5 @@ export {
   updateUserAvatar,
   upadateCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 };
